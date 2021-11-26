fc31e5b736cd2cb12f6c3f04f28ab7cf5d5ba5cb
vasudha <vasudhalakshmi.koka@gmail.com>
2021-11-26 15:32:45 +0530

    Add  a new file

---
+++ child.java
@@ -0,0 +1,14 @@
+class child extends parent{
+      public void show()
+	{
+		System.out.println("child's show");
+	}
+
+		public static  void main(String[] args) {
+		parent p=new parent();
+		p.show();
+		
+	}
+
+	}
+
