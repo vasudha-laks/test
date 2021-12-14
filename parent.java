57843a51ab6c37c544b20806023a8a17e6336801
vasudha <vasudhalakshmi.koka@gmail.com>
2021-12-14 17:12:57 +0530

    parent.java

--- parent.java
+++ parent.java
@@ -1,18 +1,18 @@
 
 public class parent {
-	public void show()
+	public void shows()
 	{
 		System.out.println("parent's show");
 	}
 	class child extends parent{
-	      public void show()
+	      public void shows()
 		{
 			System.out.println("child's show");
 		}
 
 			public  void main(String[] args) {
 			parent p=new parent();
-			p.show();
+			p.shows();
 			
 		}
 
